5bce9e7fe9e4416df64cd48804fa729fc6bb8077
SamBS <fbecerra0@ucol.mx>
2024-05-27 15:26:07 -0600

    VISUAL menu pt1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+// import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {IonicModule} from '@ionic/angular'
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePage {
   constructor() {}

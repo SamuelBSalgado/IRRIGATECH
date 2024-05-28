edcc5e0e7a99bf47eeb6fc5410d01c0ccdf0195f
SamBS <fbecerra0@ucol.mx>
2024-05-28 00:05:10 -0600

    viaje_a_patio

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 // import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import {IonicModule} from '@ionic/angular'
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private router: Router) {}
+
+  navigateToEstadoPatio() {
+    this.router.navigate(['/estado-patio']);
+  }
 }

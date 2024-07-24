6d7d99de87e8dc745d3e37be6a0c632608e0cc96
SamBS <fbecerra0@ucol.mx>
2024-07-23 20:06:42 -0600

    mensaje prueba a broker

--- src/app/estado-patio/estado-patio.page.ts
+++ src/app/estado-patio/estado-patio.page.ts
@@ -5,6 +5,9 @@ import {IonicModule} from '@ionic/angular';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Router } from '@angular/router';
 
+// import * as mqtt from 'mqtt';
+import mqtt, { MqttClient } from 'mqtt';
+
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
@@ -18,7 +21,38 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
 export class EstadoPatioPage {
   selectedOption: string = '';
 
-  constructor(private router: Router) { }
+  private client: MqttClient;
+  public message: string = 'Hola desde IONIC app';
+
+  constructor(private router: Router) {
+    this.client = mqtt.connect('wss://broker.emqx.io:8084/mqtt', {
+      clientId: 'mqttx_ce977420'
+    });
+
+    this.client.on('connect', () => {
+      console.log('Conectado al broker con éxito!');
+
+      this.client.subscribe('pruebaSerial', (err) => {
+        if (!err) {
+          console.log('Suscrito al tema pruebaSerial');
+        } else {
+          console.error('Error de suscripción: ', err);
+        }
+      });
+    });
+
+    this.client.on('message', (topic, message) => {
+      console.log(`Mensaje recibido del tema ${topic}: ${message.toString()}`);
+    });
+
+    this.client.on('error', (error) => {
+      console.error('Error de conexión', error);
+    });
+
+    this.client.on('close', () => {
+      console.log('conexión cerrada');
+    });
+  }
 
   onRadioChange(event: any) {
     this.selectedOption = event.detail.value;
@@ -27,6 +61,16 @@ export class EstadoPatioPage {
 
   go_home() {
     this.router.navigate(['/home']);
+  }
+
+  sendMessage() {
+    if (this.message.trim() !== '') {
+      this.client.publish('pruebaSerial', this.message);
+      console.log(`Mensaje enviado: ${this.message}`);
+      // this.message = '';
+    } else {
+      console.error('El mensaje está vacío');
+    }
   }
 
 }

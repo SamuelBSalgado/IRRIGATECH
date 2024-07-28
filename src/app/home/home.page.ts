c4d2717400ca17a4c7748de587b5ba6727b25a20
SamBS <fbecerra0@ucol.mx>
2024-07-27 18:54:28 -0600

    add home MODULE to add a device

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,22 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, viewChild, ViewChild } from '@angular/core';
 // import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import {IonicModule} from '@ionic/angular'
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { OverlayEventDetail } from '@ionic/core/components';
+import { IonModal } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, CommonModule, FormsModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePage {
+  @ViewChild(IonModal) modal!: IonModal;
+
+  id: string = "";
+  name: string = "";
+
   constructor(private router: Router) {}
 
   navigateToEstadoPatio() {
     this.router.navigate(['/estado-patio']);
+  }
+
+  // onWillDismiss(event: Event) {
+  //   const ev = event as CustomEvent<OverlayEventDetail<string>>;
+  //   if (ev.detail.role == 'confirm') {
+  //   }
+  // }
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+    this.id = "";
+    this.name = "";
+  }
+
+  confirm() {
+    if (this.id !== "" && this.name !== "") {
+      this.modal.dismiss(this.id, 'confirm');
+      console.log('Valores después de dismiss:', this.id, this.name);
+      this.id = "";
+      this.name = "";
+    } else {
+      console.error("Error: valores vacíos");
+    }
   }
 }
